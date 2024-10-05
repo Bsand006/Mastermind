5f1832018fbe90f340f665999f90248130084c30
bran <bran@wifi-roaming-128-4-137-25.wifi.udel.edu>
2024-10-05 11:58:58 -0400

    Mastermind board created

--- core/src/main/java/io/github/GameProject/Main.java
+++ core/src/main/java/io/github/GameProject/Main.java
@@ -1,130 +1,72 @@
 package io.github.GameProject;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class Main extends ApplicationAdapter {
-	private static final int FRAME_COLS = 8, FRAME_ROWS = 1;
-	private static final int JUMP_HEIGHT = 20;
+public class Main implements ApplicationListener {
 
-	Animation<TextureRegion> walkRightAnimation;
-	Animation<TextureRegion> walkLeftAnimation;
-	Animation<TextureRegion> waveAnimation;
-	TextureRegion idleFrame;
-	TextureRegion jumpFrame;
+	private Stage stage;
+	Texture background;
 
-	float stateTime;
-	Texture img;
-	SpriteBatch batch;
-	Sprite sprite;
+	public class MyActor extends Actor {
+		Texture circle;
+		float x = 0, y = 0;
+		public boolean started = false;
 
-	int canJump = 0;
-	float gravity = -2;
-	int jumpCounter = 0;
+		public MyActor(Texture circle) {
+			this.circle = circle;
+			setBounds(x, y, circle.getWidth(), circle.getHeight());
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha) {
+			batch.draw(circle, 200, 50); // Draws the appropriate circle at specified coordinates
+		}
+	}
 
 	@Override
 	public void create() {
-		// Loads texture (image file) into memory
-		img = new Texture(Gdx.files.internal("libgdx.png"));
+		background = new Texture(Gdx.files.internal("mastermind.png"));
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 
-		// Creates a new sprite batch and splits the it into frames based on our static
-		// variables
-		batch = new SpriteBatch();
-		TextureRegion[][] tmp = TextureRegion.split(img, img.getWidth() / FRAME_COLS, img.getHeight() / FRAME_ROWS);
+		Texture teal = new Texture(Gdx.files.internal("teal.png"));
 
-		// creating a texture region which is an array of areas on img texture this
-		// texture region has two frames and the size of those frames is taken from tmp
-		// texture regions
+		MyActor actor1 = new MyActor(teal);
+		stage.addActor(actor1);
 
-		TextureRegion[] idleFrames = new TextureRegion[1];
-		idleFrames[0] = tmp[0][0];
-		TextureRegion[] walkRightFrames = new TextureRegion[2];
-		walkRightFrames[0] = tmp[0][1];
-		walkRightFrames[1] = tmp[0][2];
-		TextureRegion[] walkLeftFrames = new TextureRegion[2];
-		walkLeftFrames[0] = tmp[0][3];
-		walkLeftFrames[1] = tmp[0][4];
-		TextureRegion[] jumpFrames = new TextureRegion[1];
-		jumpFrames[0] = tmp[0][5];
-		TextureRegion[] waveFrames = new TextureRegion[2];
-		waveFrames[0] = tmp[0][6];
-		waveFrames[1] = tmp[0][7];
-
-		// Creates a animation and assigns a set of frames as well as a time between
-		// each frame
-		walkRightAnimation = new Animation<TextureRegion>(0.25f, walkRightFrames);
-		walkLeftAnimation = new Animation<TextureRegion>(0.25f, walkLeftFrames);
-		waveAnimation = new Animation<TextureRegion>(0.25f, waveFrames);
-		idleFrame = idleFrames[0];
-		jumpFrame = jumpFrames[0];
-
-		// floating point value this is important!
-		stateTime = 0f;
-
-		sprite = new Sprite(idleFrame);
-		sprite.setPosition(50, 50);
 	}
 
 	@Override
 	public void render() {
+		stage.getBatch().begin();
+		stage.getBatch().draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		stage.getBatch().end();
+		stage.draw();
+	}
 
-		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			sprite.setRegion(waveAnimation.getKeyFrame(stateTime, true));
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			sprite.setRegion(walkRightAnimation.getKeyFrame(stateTime, true));
-			sprite.translateX(-5f);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			sprite.setRegion(walkLeftAnimation.getKeyFrame(stateTime, true));
-			sprite.translateX(5f);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-			sprite.setRegion(jumpFrame);
-			if (jumpCounter == 0 && canJump == 0) {
-				canJump = 1;
-				gravity = 8;
-			} else if (canJump == 1 && jumpCounter >= JUMP_HEIGHT) {
-				canJump = 1;
-				gravity = -2;
-			}
-		}
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
 
-		if (!Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-			gravity = -2;
-		}
+	@Override
+	public void pause() {
 
-		if (!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
-			sprite.setRegion(idleFrame);
-		}
+	}
 
-		if (canJump == 1)
-			jumpCounter++;
-		sprite.translateY(gravity);
-		if (sprite.getY() < 0) {
-			jumpCounter = 0;
-			canJump = 0;
-			sprite.setY(0);
-		}
+	@Override
+	public void resume() {
 
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stateTime += Gdx.graphics.getDeltaTime();
-		batch.begin();
-		batch.draw(sprite, sprite.getX(), sprite.getY(), 200, 180); // Draw current frame at (50, 50)
-		batch.end();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
 	}
 }
