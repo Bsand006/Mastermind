4d2befb666b6fe5fdd905ccd5c9a44146d243c6d
Brian <bsand06@pm.me>
2024-10-15 20:58:58 -0400

    All the GUI portions and code generation are complete

--- core/src/main/java/io/github/GameProject/Main.java
+++ core/src/main/java/io/github/GameProject/Main.java
@@ -1,6 +1,7 @@
 package io.github.GameProject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -69,7 +70,7 @@ public class Main extends ApplicationAdapter {
 		Image greenCircle = new Image(green);
 		Image redCircle = new Image(red);
 
-		// Blank table
+		// Blank table to help with spacing
 
 		blank.add(new Actor()).space(10);
 		blank.add(new Actor()).space(10);
@@ -107,14 +108,10 @@ public class Main extends ApplicationAdapter {
 		// Stage coordinates of the circles. This is used to determined which is clicked
 		// on in the DragListener.
 
-		board = new Board[13][4]; // Initialize array to represent main mastermind board.
+		board = new Board[4][12]; // Initialize array to represent main mastermind board.
 
 		circles.addListener(new DragListener() {
-
-			// Get the x and y coordinates of every circle relative to the stage.
-
 			float margin = 30.0f;
-
 			int selected = 0;
 
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -186,9 +183,107 @@ public class Main extends ApplicationAdapter {
 				x = event.getStageX();
 				y = event.getStageY();
 
-				Sprite draw = new Sprite(teal);
-				draw.setPosition(x, y);
-				drawedCircles.add(draw);
+				int row = 0;
+				int col = 0;
+				boolean toDraw = true;
+
+				// Determine which row on the board the user clicked on
+
+				if (y >= 224 && y <= 282) {
+					row = 1;
+				} else if (y >= 307 && y <= 361) {
+					row = 2;
+				} else if (y >= 385 && y <= 434) {
+					row = 3;
+				} else if (y >= 460 && y <= 500) {
+					row = 4;
+				} else if (y >= 527 && y <= 567) {
+					row = 5;
+				} else if (y >= 592 && y <= 646) {
+					row = 6;
+				} else if (y >= 661 && y <= 702) {
+					row = 7;
+				} else if (y >= 727 && y <= 768) {
+					row = 8;
+				} else if (y >= 794 && y <= 835) {
+					row = 9;
+				} else if (y >= 856 && y <= 901) {
+					row = 10;
+				} else if (y >= 926 && y <= 973) {
+					row = 11;
+				} else if (y >= 997 && y <= 1048) {
+					row = 12;
+				} else if (y >= 1074 && y <= 1145) {
+					row = 13;
+				} else {
+					toDraw = false;
+				}
+
+				// Determine which column the user clicked on
+
+				if (x >= 811 && x <= 958) {
+					col = 1;
+				} else if (x >= 972 && x <= 1106) {
+					col = 2;
+				} else if (x >= 1123 && x <= 1271) {
+					col = 3;
+				} else if (x >= 1288 && x <= 1441) {
+					col = 4;
+				} else if (x >= 1460 && x <= 1658) {
+					col = 5;
+				} else {
+					toDraw = false;
+				}
+
+				/*
+				 * Create a Sprite object with the cooresponding texture depending on the value
+				 * of selected. Set the position of the Sprite to the coordinates of the touchUp
+				 * event. Then add the Sprite to an ArrayList that is drawn in the render
+				 * method. Will only draw if the touchUp is within the bounds of the board. Will
+				 * also only allow red and green circles to be drawn in the 5th column. Finally
+				 * it updates the 2D board array with the appropriate color.
+				 */
+				if (toDraw) {
+					if (selected == 1 && col != 5) {
+						Sprite draw = new Sprite(teal);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.TEAL;
+					} else if (selected == 2 && col != 5) {
+						Sprite draw = new Sprite(blue);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.BLUE;
+					} else if (selected == 3 && col != 5) {
+						Sprite draw = new Sprite(orange);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.ORANGE;
+					} else if (selected == 4 && col != 5) {
+						Sprite draw = new Sprite(pink);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.PINK;
+					} else if (selected == 5 && col != 5) {
+						Sprite draw = new Sprite(purple);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.PURPLE;
+					} else if (selected == 6 && col != 5) {
+						Sprite draw = new Sprite(yellow);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+						board[col][row] = Board.YELLOW;
+					} else if (selected == 7 && col == 5) {
+						Sprite draw = new Sprite(green);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+					} else if (selected == 8 && col == 5) {
+						Sprite draw = new Sprite(red);
+						draw.setPosition(x - 10, y - 10);
+						drawedCircles.add(draw);
+					}
+				}
 
 				touchUp = true;
 			}
@@ -201,8 +296,9 @@ public class Main extends ApplicationAdapter {
 		stage.getBatch().begin(); // Begin the batch and draw the background
 		stage.getBatch().draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		if (touchUp) {
+		// If a touchUp event has just occured
 
+		if (touchUp) {
 			for (int i = 0; i < drawedCircles.size(); i++) {
 				drawedCircles.get(i).draw(stage.getBatch());
 			}
@@ -210,6 +306,33 @@ public class Main extends ApplicationAdapter {
 		stage.getBatch().end();
 		stage.draw();
 
+		logic();
+	}
+
+	private void logic() {
+		
+		// Create the random four spaced color code
+		
+		Board[] code = new Board[4];
+
+		for (int i = 0; i < code.length; i++) {
+			int rand = new Random().nextInt(6);
+			switch (rand) {
+			case 1:
+				code[i] = Board.TEAL;
+			case 2:
+				code[i] = Board.BLUE;
+			case 3:
+				code[i] = Board.ORANGE;
+			case 4:
+				code[i] = Board.PINK;
+			case 5:
+				code[i] = Board.PURPLE;
+			case 6:
+				code[i] = Board.YELLOW;
+			}
+
+		}
 	}
 
 	public void resize(int width, int height) {
